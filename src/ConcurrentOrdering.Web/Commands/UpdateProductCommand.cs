86c8c75d11ca7ae88eb7b648410023bd894aabbd
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:16:43 -0500

    Adding Commands

---
+++ src/ConcurrentOrdering.Web/Commands/UpdateProductCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcurrentOrdering.Web.Commands
+{
+    public class UpdateProductCommand
+    {
+        public int Id { get; set; }
+
+        public int AvailableUnits { get; set; }
+    }
+}
