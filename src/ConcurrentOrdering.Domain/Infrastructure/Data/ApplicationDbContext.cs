94f879a08967daffc7bd5fb9e43ea6534e761a41
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:16:54 -0500

    Create ApplicationDbContext.cs

---
+++ src/ConcurrentOrdering.Domain/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ConcurrentOrdering.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrentOrdering.Domain.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+    
+}
