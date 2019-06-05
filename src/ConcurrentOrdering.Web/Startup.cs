2a9c196beb1defdf47ed79542cd27c2ffa9db7b2
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:19:27 -0500

    Update Startup.cs

--- src/ConcurrentOrdering.Web/Startup.cs
+++ src/ConcurrentOrdering.Web/Startup.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using ConcurrentOrdering.Domain.Infrastructure.Data;
+using ConcurrentOrdering.Domain.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,8 +35,19 @@ namespace ConcurrentOrdering
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Add SQL Server
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                serverOptions => serverOptions.MigrationsAssembly("ConcurrentOrdering.Web"))
+            );
+
+            // Add repositories
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Add Automapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +55,8 @@ namespace ConcurrentOrdering
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/Error");
             }
             else
             {
