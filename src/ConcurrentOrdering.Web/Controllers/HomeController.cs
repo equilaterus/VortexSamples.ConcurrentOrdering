21226f5d077e2d9f3c75272fe8d45efaaf10bc31
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:18:42 -0500

    Update HomeController.cs

--- src/ConcurrentOrdering.Web/Controllers/HomeController.cs
+++ src/ConcurrentOrdering.Web/Controllers/HomeController.cs
@@ -5,14 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ConcurrentOrdering.Models;
+using ConcurrentOrdering.Domain.Infrastructure.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConcurrentOrdering.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductRepository _repository;
+
+        public HomeController(IMapper mapper, IProductRepository repository)
         {
-            return View();
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _repository.FindAllAsync());
         }
 
         public IActionResult Privacy()
@@ -23,7 +34,19 @@ namespace ConcurrentOrdering.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // Get the details of the exception that occurred
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionFeature is null)
+                return StatusCode(500, "Unknown error");
+
+            if (!exceptionFeature.Path.Contains("/api/", StringComparison.InvariantCultureIgnoreCase))
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            
+            if (exceptionFeature.Error is DbUpdateConcurrencyException)
+                return Json(new { Error = "Concurrency exception", AllowRetry = true });
+            else
+                return Json(new { exceptionFeature.Error });
         }
     }
 }
