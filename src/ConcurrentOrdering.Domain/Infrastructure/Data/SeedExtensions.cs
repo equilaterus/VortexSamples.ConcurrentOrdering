1638acb040009f62547081499e668bf071b40266
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:17:29 -0500

    Create SeedExtensions.cs

---
+++ src/ConcurrentOrdering.Domain/Infrastructure/Data/SeedExtensions.cs
@@ -0,0 +1,28 @@
+﻿using ConcurrentOrdering.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrentOrdering.Domain.Infrastructure.Data
+{
+    public static class SeedExtensions
+    {
+        public static readonly Product[] ProductsSeed = new Product[] {
+            new Product
+            {
+                Id = 1,
+                Name = "Thimbleweed Park Diskette",
+                AvailableUnits = 3,
+                ImageUrl = "https://i.ytimg.com/vi/zRmh1aFHHQQ/hqdefault.jpg"
+            }
+        };
+
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+               ProductsSeed
+            );
+        }
+    }
+}
