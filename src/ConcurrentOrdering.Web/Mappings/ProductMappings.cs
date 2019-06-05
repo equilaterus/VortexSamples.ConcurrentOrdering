465a62e70cfd4ff42624d55a3ac5c161bb5408f2
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:19:13 -0500

    Create ProductMappings.cs

---
+++ src/ConcurrentOrdering.Web/Mappings/ProductMappings.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ConcurrentOrdering.Domain.Models;
+using ConcurrentOrdering.Web.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcurrentOrdering.Web.Mappings
+{
+    public class ProductMappings : Profile
+    {
+        public ProductMappings()
+        {
+            CreateMap<UpdateProductCommand, Product>();
+        }
+    }
+}
