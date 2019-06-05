671548a1fd3a89e18348a3ae86d064d4ae37af82
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:17:24 -0500

    Adding Product Repository

---
+++ src/ConcurrentOrdering.Domain/Infrastructure/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConcurrentOrdering.Domain.Models;
+
+namespace ConcurrentOrdering.Domain.Infrastructure.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> FindAllAsync();
+        Task<Product> FindByIdAsync(int id);
+        Task<Product> UpdateAsync(Product product);
+    }
+}
---
+++ src/ConcurrentOrdering.Domain/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using ConcurrentOrdering.Domain.Infrastructure.Data;
+using ConcurrentOrdering.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentOrdering.Domain.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> FindAllAsync()
+        {
+            return await _context.Products.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Product> FindByIdAsync(int id)
+        {
+            return await _context.Products.AsNoTracking().FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
