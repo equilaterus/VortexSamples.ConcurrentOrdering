1b622d7de5c0135993e0b02c5ae4253294eb2404
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 02:24:14 -0500

    Moving Models to Domain Project

---
+++ src/ConcurrentOrdering.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcurrentOrdering.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int AvailableUnits { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
