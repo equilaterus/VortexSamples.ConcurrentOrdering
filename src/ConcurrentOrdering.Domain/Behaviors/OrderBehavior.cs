0919a95b26a45180d61de64106e867a168f68987
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:17:39 -0500

    Create OrderBehavior.cs

---
+++ src/ConcurrentOrdering.Domain/Behaviors/OrderBehavior.cs
@@ -0,0 +1,23 @@
+﻿using ConcurrentOrdering.Domain.Models;
+using Equilaterus.Vortex;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrentOrdering.Domain.Behaviors
+{
+    public static class OrderBehavior
+    {
+        public static Maybe<Product> TryOrder(Product product, int quantity)
+        {
+            if (quantity > product.AvailableUnits)
+            {
+                return new Maybe<Product>();
+            }
+
+            var newProduct = new Product(product);
+            newProduct.AvailableUnits -= quantity;
+            return new Maybe<Product>(newProduct);            
+        }
+    }
+}
