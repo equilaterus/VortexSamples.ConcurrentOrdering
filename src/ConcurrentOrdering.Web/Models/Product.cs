62ced750893ab4048195b346fe953c20862bb481
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 02:13:53 -0500

    Updating folder structure

---
+++ src/ConcurrentOrdering.Web/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConcurrentOrdering.Web.Models
+{
+    public class Product
+    {
+    }
+}
