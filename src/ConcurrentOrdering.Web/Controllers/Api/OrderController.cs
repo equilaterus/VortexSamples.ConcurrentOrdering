15bb977d3e3c4c1e3e2b387e378f30e3db70c920
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 16:18:34 -0500

    Adding Controllers

---
+++ src/ConcurrentOrdering.Web/Controllers/Api/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ConcurrentOrdering.Domain.Behaviors;
+using ConcurrentOrdering.Domain.Infrastructure.Repositories;
+using ConcurrentOrdering.Web.Commands;
+using Microsoft.AspNetCore.Mvc;
+using Equilaterus.Vortex;
+using ConcurrentOrdering.Models;
+using System.Diagnostics;
+
+namespace ConcurrentOrdering.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _repository;
+
+        public OrderController(IMapper mapper, IProductRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Order(OrderCommand orderCommand)
+        {
+            return await
+                // Try to generate an order
+                from maybeOrder in
+                    from product in _repository.FindByIdAsync(orderCommand.ProductId)
+                    select OrderBehavior.TryOrder(product, orderCommand.Quantity)
+
+                // Update the DB
+                from result in maybeOrder.AwaitSideEffect(_repository.UpdateAsync)
+                
+                // Return results
+                select result.Match<IActionResult>(
+                    Ok,
+                    StatusCode(500));
+        }
+    }
+}
---
+++ src/ConcurrentOrdering.Web/Controllers/Api/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ConcurrentOrdering.Domain.Behaviors;
+using ConcurrentOrdering.Domain.Infrastructure.Repositories;
+using ConcurrentOrdering.Web.Commands;
+using Microsoft.AspNetCore.Mvc;
+using Equilaterus.Vortex;
+using ConcurrentOrdering.Models;
+using System.Diagnostics;
+using ConcurrentOrdering.Domain.Models;
+
+namespace ConcurrentOrdering.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _repository;
+
+        public ProductController(IMapper mapper, IProductRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductCommand updateProductCommand)
+        {
+            return await
+                // Try to generate an order
+                from maybeProduct in
+                    from product in _repository.FindByIdAsync(updateProductCommand.Id)
+                    select _mapper.MaybeMap(updateProductCommand, product)
+
+                // Update the DB
+                from result in maybeProduct.AwaitSideEffect(_repository.UpdateAsync)
+                
+                // Return results
+                select result.Match<IActionResult>(
+                    Ok, 
+                    StatusCode(500, "No available units"));
+        }
+    }
+}
