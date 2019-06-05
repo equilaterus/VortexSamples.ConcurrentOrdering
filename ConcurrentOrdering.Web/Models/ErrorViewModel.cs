d31bf87ac2effc651706e34574dfac11488de67f
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 02:02:06 -0500

    Creating VS project

---
+++ ConcurrentOrdering.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConcurrentOrdering.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
