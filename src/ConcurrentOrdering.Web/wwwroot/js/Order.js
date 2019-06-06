4bd3cddaa9a401519ee76a38ab669a629c29ebb4
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 21:07:45 -0500

    Adding bundler and refatoring JS files

---
+++ src/ConcurrentOrdering.Web/wwwroot/js/Order.js
@@ -0,0 +1,71 @@
+﻿// Order JS
+
+// Constants
+const SUCCESS = 'bg-success';
+const ERROR = 'bg-danger';
+const WARNING = 'bg-warning';
+
+const ORDER_URL = `${window.location.origin}/api/order/`;
+const ORDER_METHOD = 'POST';
+
+const PRODUCT_RESET_URL = `${window.location.origin}/api/product/`;
+const PRODUCT_RESET_METHOD = 'POST';
+
+const ordering = {};
+
+ordering.SubmitOrder = function (product, userId) {
+    $.ajax({
+        method: ORDER_METHOD,
+        url: ORDER_URL,
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({ ProductId: product, Quantity: 1 })
+    })
+    .done(function (result) {
+        toasts.Notify(`User ${userId}`, 'Order completed successfully', SUCCESS);
+        ordering.UpdateAvailableUnits(result);
+    })
+    .fail(function (err, textStatus) {
+        console.log(err);
+        if (err.responseJSON.allowRetry) {
+            toasts.Notify(
+                `ERROR: User ${userId}`,
+                'Order failed because another user already took the item. Please retry.',
+                WARNING
+            );
+        } else {
+            toasts.Notify(
+                `ERROR: User ${userId}`,
+                err.responseJSON.error,
+                ERROR
+            );
+        }
+    });
+};
+
+ordering.Order = function (product, concurrentTimes) {
+    for (let i = 0; i < concurrentTimes; ++i) {
+        this.SubmitOrder(product, i + 1);
+    }
+};
+
+ordering.SubmitReset = function (product, quantity) {
+    $.ajax({
+        method: PRODUCT_RESET_METHOD,
+        url: PRODUCT_RESET_URL,
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({ Id: product, AvailableUnits: quantity })
+    })
+    .done(function (result) {
+        toasts.Notify(`Reset units`, 'Operation completed successfully', SUCCESS);
+        ordering.UpdateAvailableUnits(result);
+    })
+    .fail(function (err, textStatus) {
+        toasts.Notify('ERROR: Product update', err.responseJSON.error, ERROR);
+    });
+};
+
+ordering.UpdateAvailableUnits = function (product) {
+    $(`#units_${product.id}`).text(product.availableUnits);
+};
---
+++ src/ConcurrentOrdering.Web/wwwroot/js/Toasts.js
@@ -0,0 +1,27 @@
+﻿// Toast behavior
+const TOAST_AREA_SELECTOR = '#toast-area';
+
+const toasts = {};
+toasts.Id = 0;
+toasts.Notify = function (title, message, bgClass) {
+    const toastId = ++this.Id;
+    $(TOAST_AREA_SELECTOR).append(
+        `<div id="toast_${toastId}" class="toast " role="alert" aria-live="assertive" aria-atomic="true" data-delay="10000" data-autohide="true">
+          <div class="toast-header ${bgClass} text-white">
+            <strong class="mr-auto">${title}</strong>
+            <small>Just now</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="toast-body">
+            ${message}
+          </div>
+        </div>`
+    );
+    $(`#toast_${toastId}`).toast('show');
+
+    $(`#toast_${toastId}`).on('hidden.bs.toast', function () {
+        $(this).toast('dispose');
+    });
+};
