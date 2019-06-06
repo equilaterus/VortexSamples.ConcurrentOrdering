f4a4a3e2547e3dc0a25b7cec06ad7ed7fc5c5570
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-06-05 19:36:41 -0500

    Toasts added

--- src/ConcurrentOrdering.Web/wwwroot/js/site.js
+++ src/ConcurrentOrdering.Web/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Order JS
+﻿// Constants
+const SUCCESS = 'bg-success';
+const ERROR = 'bg-danger';
+const WARNING = 'bg-warning';
+
+// Utils
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+// Order JS
 const ORDER_URL = `${window.location.origin}/api/order/`;
 const ORDER_METHOD = 'POST';
 
@@ -16,13 +28,24 @@ ordering.SubmitOrder = function (product, userId) {
         data: JSON.stringify({ ProductId: product, Quantity: 1 })
     })
     .done(function (result) {        
-        console.log(result);
+        toasts.Notify(`User ${userId}`, 'Order completed successfully', SUCCESS);
         ordering.UpdateAvailableUnits(result);
     })
     .fail(function (err, textStatus) {
-        alert(`User ${userId} failed to complete order. ${JSON.stringify(err.responseJSON)}`);
-        console.error(err);
-        console.error(textStatus);
+        let errorMsg;
+        if (err.responseJSON.allowRetry) {
+            toasts.Notify(
+                `ERROR: User ${userId}`, 
+                'Order failed because another user already took the item. Please retry.',
+                WARNING
+            );
+        } else {
+            toasts.Notify(
+                `ERROR: User ${userId}`, 
+                'Order failed because there are no available units.',
+                ERROR
+            );
+        }        
     });
 };
 
@@ -41,18 +64,45 @@ ordering.SubmitReset = function (product, quantity) {
         data: JSON.stringify({ Id: product, AvailableUnits: quantity })
     })
     .done(function (result) {
+        toasts.Notify(`Reset units`, 'Operation completed successfully', SUCCESS);
         ordering.UpdateAvailableUnits(result);
     })
     .fail(function (err, textStatus) {
-        alert(`Product ${product} failed to update`);
-        console.error(err);
-        console.error(textStatus);
+        alert(`Product ${product} failed to update`, ERROR);
     });
-}
+};
 
 ordering.UpdateAvailableUnits = function (product) {
     $(`#units_${product.id}`).text(product.availableUnits);
-}
+};
+
+// Toast behavior
+const TOAST_AREA_SELECTOR = '#toast-area'; 
+
+const toasts = {};
+toasts.Id = 0;
+toasts.Notify = function (title, message, bgClass) {
+    const toastId = ++this.Id;
+    $(TOAST_AREA_SELECTOR).append(
+        `<div id="toast_${toastId}" class="toast " role="alert" aria-live="assertive" aria-atomic="true" data-delay="10000" data-autohide="true">
+          <div class="toast-header ${bgClass} text-white">
+            <strong class="mr-auto">${title}</strong>
+            <small>11 mins ago</small>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="toast-body">
+            ${message}
+          </div>
+        </div>`
+    );
+    $(`#toast_${toastId}`).toast('show');
+
+    $(`#toast_${toastId}`).on('hidden.bs.toast', function () {
+        $(this).toast('dispose');
+    });
+};
 
 // jQuery events
 $(function () {
